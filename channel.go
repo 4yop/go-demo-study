351cc0a06e9d72da28cc61d9c34a403de9350e7f
4yop <1131559748@qq.com>
2021-08-17 23:14:38 +0800

    'study-go'

--- channel.go
+++ channel.go
@@ -6,14 +6,18 @@ import (
 
 func main() {
 	ch := make(chan int)
-	fmt.Println(ch)
+
 
 	ch1 := make(chan int)
-	fmt.Println(ch1)
+
+
+
 	go func() {
 		ch <- 1
 		//value := <-ch
 		ch1 <- 1
+		fmt.Println(<-ch)
+
 	}()
 
 	data := <-ch
