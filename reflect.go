83b80f07d7c748de4bfd983ff57aad3ac846d8ca
lzh <1131559748@qq.com>
2021-08-18 15:53:56 +0800

    'study-go'

--- reflect.go
+++ reflect.go
@@ -5,11 +5,19 @@ import (
 	"reflect"
 )
 
+type people struct {
+	name string
+	age int
+}
+
 func main()  {
 	var a float64 = 123
 	value := reflect.ValueOf(a)
 	fmt.Println(value)
 	i3 := value.Interface().(float64)
-	
+
+
+	reflect.ValueOf(people)
+
 	fmt.Println(i3)
 }
