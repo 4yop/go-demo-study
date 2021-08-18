4b6c423428ff13114523ef3aa47f10fedac529ff
lzh <1131559748@qq.com>
2021-08-18 11:45:59 +0800

    'study-go'

--- channel.go
+++ channel.go
@@ -3,25 +3,34 @@ package main
 import (
 	"fmt"
 )
-var ch
+
 func main() {
 	ch := make(chan int)
 	fmt.Println(ch)
 
-	ch1 := make(chan int)
-	fmt.Println(ch1)
+
 	go func() {
 		ch <- 1
-		//value := <-ch
-		ch1 <- 1
 	}()
 
 	fmt.Println(<-ch)
 
-	fmt.Println(<-ch1)
+	ch1 := make(chan int)
+
+	go msg(ch1)
+
+	for k, v := range ch1 {
+		fmt.Println("ch",k,v)
+	}
+
+
+
 }
 
 
-func msg () {
-	ch
+func msg (ch chan int) {
+	for i := 0; i < 10; i++ {
+		ch <- i
+	}
+	close(ch)
 }
