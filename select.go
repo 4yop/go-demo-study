e234345f8531afdb9d7db9cd7c2437db3ba0563f
lzh <1131559748@qq.com>
2021-08-18 12:40:50 +0800

    'study-go'

--- select.go
+++ select.go
@@ -10,18 +10,18 @@ func main()  {
 	ch2 := make(chan int)
 
 	go func() {
-		time.Sleep(3*time.Second)
 		ch1 <- 99
+		time.Sleep(1*time.Second)
 	}()
 
 	select {
 		case <-ch1:
-			fmt.Println("<-ch1",<-ch1)
+			fmt.Println("<-ch1")
 		case <-ch2:
-			fmt.Println("<-ch2",<-ch2)
+			fmt.Println("<-ch2")
 		case <-time.After(3*time.Second):
 			fmt.Println("3333")
-		default:
-			fmt.Println("default")
+		//default:
+		//	fmt.Println("default")
 	}
 }
