4592f95264019feb4a03175e884ea624de542f06
lzh <1131559748@qq.com>
2021-08-18 12:36:21 +0800

    'study-go'

--- select.go
+++ select.go
@@ -1,11 +1,18 @@
 package main
 
+import "fmt"
+
 func main()  {
 	ch1 := make(chan int)
 	ch2 := make(chan int)
 
 	select {
-	case <-ch1:
-		
+		case <-ch1:
+			fmt.Println("<-ch1",<-ch1)
+		case <-ch2:
+			fmt.Println("<-ch2",<-ch2)
+		case 
+		default:
+			fmt.Println("default")
 	}
 }
