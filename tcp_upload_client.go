77f7a4711c321f47fe4b3c5fc3508f7f8ea768b9
lzh <1131559748@qq.com>
2021-12-25 11:17:07 +0800

    go study

--- tcp_upload_client.go
+++ tcp_upload_client.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"net"
+)
+
+
+
 func main() {
-	
+
+	conn,err := net.Dial("tcp","127.0.0.1:789")
+	if err != nil {
+		fmt.Println(conn)
+	}
+	defer conn.Close()
 }
